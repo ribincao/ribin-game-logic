d92f756ea56010d90230a408c3fb9a336ecabd5b
bud-caolibin <caolibin@budcreate.io>
2023-01-09 12:12:15 +0800

    feat:补充关闭连接回调函数

--- handler/connection_handler.go
+++ handler/connection_handler.go
@@ -1,7 +1,15 @@
 package handler
 
-import "github.com/ribincao/ribin-game-server/network"
+import (
+	"github.com/ribincao/ribin-game-server/memory"
+	"github.com/ribincao/ribin-game-server/network"
+)
 
 func OnClose(conn *network.WrapConnection) {
-
+	roomId := memory.GetRoomIdByPlayerId(conn.PlayerId)
+	if roomId == "" {
+		return
+	}
+	// TODO: room kill player
+	// TODO: Broadcast
 }
